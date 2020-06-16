3fc4ed1032451938f5f394252666f20d30c9504c
saltkeeper1@gmail.com <saltkeeper1@gmail.com>
2020-06-17 02:34:44 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using CodeBlogStack.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBlogStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var easyStack = new EasyStack<int>();
+            Stack<double> stack = new Stack<double>();
+
+
+
+            easyStack.Push(1);
+            easyStack.Push(2);
+            easyStack.Push(3);
+
+            var item1 = easyStack.Pop();
+            var item2 = easyStack.Peek();
+            Console.WriteLine(item1);
+            Console.WriteLine(item2);
+            Console.ReadLine();
+
+            var linkedStack = new LinkedStack<int>();
+            linkedStack.Push(10);
+            linkedStack.Push(20);
+            linkedStack.Push(30);
+            
+            Console.WriteLine(linkedStack.Peek());
+            Console.WriteLine(linkedStack.Pop());
+            Console.WriteLine(linkedStack.Pop());
+            Console.WriteLine(linkedStack.Peek());
+            Console.ReadLine();
+
+            var arrayStack = new ArrayStack<int>(5);
+            Console.WriteLine(arrayStack.Count);
+            arrayStack.Push(100);
+            Console.WriteLine(arrayStack.Count);
+            arrayStack.Push(200);
+            arrayStack.Push(300);
+            arrayStack.Push(400);
+            arrayStack.Push(500);
+            Console.WriteLine(arrayStack.Peek());
+            Console.WriteLine(arrayStack.Pop());
+            Console.WriteLine(arrayStack.Pop());
+            Console.WriteLine(arrayStack.Peek());
+            
+            Console.ReadLine();
+        }
+    }
+}
